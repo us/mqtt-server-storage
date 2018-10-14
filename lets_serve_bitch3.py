3478b315b81ac4e2b30ff4a290053e91f472af1b
us <rahmetsaritekin@gmail.com>
2018-10-15 02:01:05 +0300

    Centered div

--- lets_serve_bitch3.py
+++ lets_serve_bitch3.py
@@ -1,21 +1,5 @@
+from flask import Flask, jsonify, g, request, render_template
 import sqlite3
-conn = sqlite3.connect('IoT.db')
-c = conn.cursor()
-
-#a = c.fetchall()
-
-
-
-def take_data(data):
-	conn = sqlite3.connect('IoT.db')
-	c = conn.cursor()
-	c.execute("SELECT * FROM DHT22_Humidity_Data LIMIT 20")
-	humidity_data = c.fetchall()
-	c.execute("SELECT * FROM DHT22_Temperature_Data LIMIT 20")
-	temperature_data = c.fetchall()
-	conn.close()
-	return temperature_data, humidity_data
-
 
 
 def parse(data_name):
@@ -29,38 +13,20 @@ def parse(data_name):
 		data = c.fetchall()
 	conn.close()
 	return data
-'''
-	parsed = {}
-	for i in data:
-		i_d = i[0]
-		SensorID = i[1]
-		date = i[2]
-		data1 = i[3]
-		
-		parsed[i_d] = []
-		parsed[i_d].append({
-			"SensorID" : SensorID,
-			"date" : date,
-			data_name : data1
-			})
-#	print(type(parsed))
-#	print(parsed)
-'''
-
-#print(parse(humidity_data))
 
 
 
-from flask import Flask, jsonify, g, request, render_template
 app = Flask(__name__)
 app.config["JSON_SORT_KEYS"] = False
 
 @app.route('/')
 def hello():
 	return """ 
+		<div>
 		<H1>Hello Motherfuckers<H1>
 		<a href="/temperature">Temperature</a>
 		<a href="/humidity">Humidity</a>
+		</div>
 	"""
 
 @app.route('/humidity')
